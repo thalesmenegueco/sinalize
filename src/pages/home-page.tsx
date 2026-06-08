f419b5711a1819e72eea3badea3bdbbb4bf2f376
thalesmenegueco <thales.menegueco@gmail.com>
2026-06-08 15:01:39 -0300

    fix: wrong image paths

--- src/pages/home-page.tsx
+++ src/pages/home-page.tsx
@@ -10,7 +10,7 @@ function HomePage() {
             <PageContent
                 pageTitle="Aprenda Libras na Prática"
                 description="Sua plataforma minimalista e interativa para aprender a sinalizar e treinar sua comunicação em Libras"
-                imgUrl="../src/assets/img-home.png">
+                imgUrl="../assets/img-home.png">
                 <VideoExplainer
                     videoSrc="https://pub-53c058da4b9543a8b580f5adffb6a867.r2.dev/how-to_videos/flashcard-page_COMO_USAR.mp4"
                     title="Como Funciona esta Página?"
